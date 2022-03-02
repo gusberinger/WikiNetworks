35d56fd7e1a2dbd0044bd4b466e65fef5b8ef6b9
Gus Beringer <gusberinger@gmail.com>
2022-03-02 04:00:08 +0000

    removed compression from conversion

--- sparse_graph/convert_page_ids_to_matrix_index.py
+++ sparse_graph/convert_page_ids_to_matrix_index.py
@@ -4,11 +4,8 @@ from helpers import *
 import pickle
 import logging
 from database import Database
-import gzip
 
 logging.getLogger().setLevel(logging.DEBUG)
-
-
 
 db = Database(DATABASE_PATH)
 pages_df = pd.read_sql("SELECT id FROM pages", db.sdow_conn)
@@ -17,9 +14,8 @@ nodes_list = SortedList(pages_df["id"])
 logging.info("converted to SortedList")
 
 
-with gzip.open(MATRIX_INDEX_PATH, "wb") as f:
+with open(MATRIX_INDEX_PATH, "wb") as f:
     pickle.dump(nodes_list, f)
 
-exit()
-
+logging.info("created node list file")
 
