1fb48ffc248521abb094684cc9ba1a4f3aa82d54
Gus Beringer <gusberinger@gmail.com>
2022-03-02 20:06:46 +0000

    dump chuncks of sparse matrices to be summed

--- sparse_graph/create_sparse_graph.py
+++ sparse_graph/create_sparse_graph.py
@@ -1,9 +1,10 @@
+import copy
 import logging
 import pickle
 from helpers import *
 import pandas as pd
 import numpy as np
-from scipy.sparse import lil_matrix
+import scipy.sparse
 from tqdm import tqdm
 from multiprocessing import Pool
 
@@ -35,8 +36,9 @@ def wiki_df_to_matrix_df(df):
     return df
 
 
-def dense_df_to_sparse(df):
-    mat = lil_matrix((MATRIX_SIZE, MATRIX_SIZE))
+def dense_df_to_sparse(df, template):
+    mat = copy.copy(template)
+    # mat = lil_matrix((MATRIX_SIZE, MATRIX_SIZE))
     for row in df.itertuples():
         mat[row.source, row.target] = 1
     mat = mat.tocsr()
@@ -61,33 +63,33 @@ def parallelize_matrix_creation(df, n_cores=12):
     return mat
 
 
-
-
 if __name__ == "__main__":
     # logging.info("Creating empty lil_matrix")
-    # EMPTY_MAT = scipy.sparse.lil_matrix((MATRIX_SIZE, MATRIX_SIZE))
+    EMPTY_MAT = scipy.sparse.lil_matrix((MATRIX_SIZE, MATRIX_SIZE))
 
     logging.info("Loading edge list dataframe")
-
     # there are approximately 562,142,540 total rows. For testing purposes it is time consuming to load all of them.
-    edge_df = pd.read_csv(EDGE_LIST_PATH, 
-        delimiter=',', 
-        header=None, 
+    for i, edge_df_chunk in enumerate(pd.read_csv(EDGE_LIST_PATH, 
+        delimiter = ',', 
+        header = None, 
         names = ["source", "target"],
-        dtype=np.uint32, 
-        nrows = 10 ** 7) 
+        dtype = np.uint32,
+        chunksize = 562142540 // 20)):
+        # nrows = 10 ** 8)
 
-    logging.info("converting wiki ids to matrix indices")
-    lil_df = parallelize_dataframe(edge_df, wiki_df_to_matrix_df)
-    edge_df = None
-    logging.info("done.")
+        logging.info(f"{i} - converting wiki ids to matrix indices")
+        lil_df = parallelize_dataframe(edge_df_chunk, wiki_df_to_matrix_df)
+        edge_df_chunk = None
+        logging.info("done.")
 
 
-    mat = dense_df_to_sparse(lil_df)
-    # mat = parallelize_matrix_creation(lil_df)
-    # print(type)
+        mat = dense_df_to_sparse(lil_df, EMPTY_MAT)
+        # mat = parallelize_matrix_creation(lil_df)
+        # print(type)
 
-    logging.info("created sparse matrix")
-    logging.info("done.")
-    with open(SPARSE_MATRIX_PATH, "wb") as f:
-        pickle.dump(mat, f)
+        logging.info("created sparse matrix")
+        logging.info("done.")
+        with open(SPARSE_MATRIX_PATH.parent.joinpath(f"sparse_mat_{i}.pickle"), "wb") as f:
+            pickle.dump(mat, f)
+
+        mat = None
