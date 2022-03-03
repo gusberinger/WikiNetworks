161138189630fbf5945401a1c9f4b737061bce89
Gus Beringer <gusberinger@gmail.com>
2022-03-02 19:12:58 -0600

    removed extra step of generating edge list

--- sparse_graph/convert_page_ids_to_matrix_index.py
+++ sparse_graph/convert_page_ids_to_matrix_index.py
@@ -1,11 +1,9 @@
-import copy
 import logging
+from database import Database
 import pickle
 from helpers import *
 import pandas as pd
 import numpy as np
-import scipy.sparse
-from tqdm import tqdm
 from multiprocessing import Pool
 
 logging.basicConfig(
@@ -25,9 +23,14 @@ def id_key(wiki_id : int) -> int:
         return np.nan
 
 
-def wiki_df_to_matrix_df(df):
-    df['source'] = df['source'].apply(id_key)
-    df['target'] = df['target'].apply(id_key)
+
+
+def links_df_to_matrix_df(df):
+    df["outgoing_links"] = df["outgoing_links"].apply(lambda str: str.split("|"))
+    df = df.explode("outgoing_links")
+    df = df.astype(np.uint32)
+    df['id'] = df['id'].apply(id_key)
+    df['outgoing_links'] = df['outgoing_links'].apply(id_key)
     df = df.dropna()
     df = df.astype(np.uint32)
     return df
@@ -46,19 +49,33 @@ if __name__ == "__main__":
 
     # logging.info("Loading edge list dataframe")
     # there are approximately 562,142,540 total rows. For testing purposes it is time consuming to load all of them.
-    for i, edge_df_chunk in enumerate(pd.read_csv(EDGE_LIST_PATH, 
-        delimiter = ',', 
-        header = None, 
-        names = ["source", "target"],
-        dtype = np.uint32,
-        chunksize = 562142540 // 20)):
-        # nrows = 10 ** 8)
 
 
-        logging.info(f"{i} - converting wiki ids to matrix indices")
-        lil_df = parallelize_dataframe(edge_df_chunk, wiki_df_to_matrix_df)
+    db = Database(DATABASE_PATH)
+
+
+    # Multiprocessing doesn't work correctly for chunks greater than 2GB
+    LINKS_TABLE_SIZE = db.sdow_conn.execute("SELECT COUNT() FROM links").fetchone()[0]
+    chunksize = LINKS_TABLE_SIZE // 6
+
+    # order by descending so we can find out memory issues right away. 
+    query = "SELECT id, outgoing_links FROM links where outgoing_links_count > 0 ORDER BY outgoing_links_count DESC"
+
+    for i, edge_df_chunk in enumerate(pd.read_sql(query,
+        db.sdow_conn,
+        chunksize = chunksize)):
+
+        lil_df = parallelize_dataframe(edge_df_chunk, links_df_to_matrix_df, n_cores=6)
         edge_df_chunk = None
         lil_df.to_parquet(DUMP_PATH.joinpath(f"lil_df-{i}.parquet"))
+        logging.info("Processed chunck {i}.")
 
-
-        
+    
+    glob = list(DUMP_PATH.glob("lil_df-*.parquet"))
+    logging.info(f"Combining {len(glob)} parquet files.")
+    df = pd.concat((pd.read_parquet(path) for path in glob))
+    logging.info(f"Saving combined files.")
+    df.to_parquet(DUMP_PATH.joinpath("lil_df.parquet"))
+    logging.info("Removing sepearte parquet files.")
+    for path in glob:
+        path.unlink()
