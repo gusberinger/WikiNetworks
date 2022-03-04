9a5d7976dd8263430ecd8f12737fd65852a1273a
Gus Beringer <gusberinger@gmail.com>
2022-03-04 21:38:10 +0000

    created nodelist class

--- sparse_graph/helpers.py
+++ sparse_graph/helpers.py
@@ -4,6 +4,10 @@ Helper classes and methods.
 
 from pathlib import Path
 import sqlite3
+from typing import List
+import pandas as pd
+import numpy as np
+
 
 WIKIPEDIA_API_URL = 'https://en.wikipedia.org/w/api.php'
 ROOT_PATH = Path(__file__).parent.parent
@@ -18,3 +22,38 @@ class Database(object):
 			raise IOError('Specified SQLite file "{0}" does not exist.'.format(DATABASE_PATH))
 		self.sdow_conn = sqlite3.connect(DATABASE_PATH, check_same_thread=False)
 		self.sdow_cursor = self.sdow_conn.cursor()
+
+
+class NodeList(object):
+	def __init__(self) -> None:
+		db = Database()
+
+		pages_df = pd.read_sql("SELECT id FROM pages where is_redirect=0", db.sdow_conn)
+		node_list = sorted(pages_df["id"])
+		node_dict = {k:v for k, v in enumerate(node_list)}
+
+		self.node_dict = node_dict
+
+	def find_index(self, article_id : int, safe = True):
+		if safe:
+			try:
+				return self.labels.index(article_id)
+			except ValueError:
+				return np.nan
+		else:
+			return self.labels.index(article_id) 
+
+
+	def find_article_id(self, index : int):
+		return self.labels[index]
+
+
+	def remove_indices(self, indices : List[int]) -> None:
+		"""Remove values from NodeList by index in place. The indexes are shifted down so the structure is maintained.
+		That is, if index 2 is removed from [(0, A), (1, B), (2, C), (3, D)), (4, E), (5, F)] the new list becomes:
+			[(0, A), (1, B), (2, D), (3, E), (4, F)]
+		"""
+		node_list = self.node_dict.values()
+		node_list = np.delete(node_list, indices)
+		self.node_dict = {k:v for k, v in enumerate(node_list)}
+
