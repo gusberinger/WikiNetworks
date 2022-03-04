b5a41bcd62122a7e132ba89c1450f79e1c7451eb
Gus Beringer <berin013@umn.edu>
2022-03-04 15:10:35 -0600

    removed logs

--- sparse_graph/create_nodes_list.py
+++ sparse_graph/create_nodes_list.py
@@ -2,22 +2,15 @@ import pandas as pd
 from sortedcontainers import SortedList
 from helpers import *
 import pickle
-import logging
-from database import Database
-
-logging.getLogger().setLevel(logging.DEBUG)
-
-db = Database(DATABASE_PATH)
-
-pages_df = pd.read_sql("SELECT id FROM pages where is_redirect=0", db.sdow_conn)
-logging.info("loaded pages dataframe")
-
-nodes_list = SortedList(pages_df["id"])
-logging.info("converted to SortedList")
 
 
-with open(NODES_LIST_PATH, "wb") as f:
-    pickle.dump(nodes_list, f)
+if __name__ == "__main__":
 
-logging.info("created node list file")
+    db = Database()
+
+    pages_df = pd.read_sql("SELECT id FROM pages where is_redirect=0", db.sdow_conn)
+    nodes_list = SortedList(pages_df["id"])
+
+    with open(NODES_LIST_PATH, "wb") as f:
+        pickle.dump(nodes_list, f)
 
