5b3140947aab456cd272e22c6a9fdfc17feba676
Gus Beringer <berin013@umn.edu>
2022-03-15 14:06:55 -0500

    created sparsegraph class

--- sparse_graph/create_connected_subgraph.py
+++ sparse_graph/create_connected_subgraph.py
@@ -1,6 +1,6 @@
 import logging
 from re import sub
-from typing import List, Optional
+from typing import Any, List, Optional
 from scipy import sparse
 from scipy.sparse import load_npz
 import numpy as np
@@ -29,53 +29,68 @@ def _delete_from_csr(mat, indices : List[int]):
     mask[indices] = False
     return mat[mask][:,mask]
 
+class SparseGraph:
 
-def get_largest_component(matrix, labels, 
-    directed : bool = True, connection : str = "strong"):
-    """
-    Parameters
-    ---
-    matrix:
-        The sparse adjacency matrix of the graph.
-    labels:
-        The labels of each node in the graph. The index of the list corresponds to the index in the adjacency matrix.
-    direct:
-        If ``True`` the graph is treated as directed. 
-    connection:
-        If ``"strong"``, the connected components will all be strongly connected together, if ``"weak"`` the connected components will be weakly connected. If ``directed == False`` the parameter is ignored.
+    def __init__(self, adjacency, labels : list[Any] | NodeList) -> None:
+        self.adjacency = adjacency
+        self.labels = NodeList(labels) if not isinstance(labels, NodeList) else labels
+        self.in_degrees = None
+        self.out_degrees = None
 
-    Returns
-    ---
-    sub_matrix:
-        The sparse adjacency matrix of the largest strongly connected subgraph.
-    sub_labels:
-        The labels of each node in the subgraph.
-    """
-    _, component_labels = sparse.csgraph.connected_components(matrix, directed=directed, connection=connection)
-    unique_component_labels, count = np.unique(component_labels, return_counts=True)
-    largest = unique_component_labels[np.argmax(count)]
-    indices = np.where(component_labels == largest)[0]
-    unconnected_indices = list(set(range(mat.shape[0])) - set(indices))
-    labels.remove_indices(unconnected_indices)
-    sub_matrix = _delete_from_csr(mat, unconnected_indices)
-    return sub_matrix, labels
+    def get_largest_component(self, directed : bool = True, connection : str = "strong"):
+        """
+        Parameters
+        ---
+        matrix:
+            The sparse adjacency matrix of the graph.
+        labels:
+            The labels of each node in the graph. The index of the list corresponds to the index in the adjacency matrix.
+        direct:
+            If ``True`` the graph is treated as directed. 
+        connection:
+            If ``"strong"``, the connected components will all be strongly connected together, if ``"weak"`` the connected components will be weakly connected. If ``directed == False`` the parameter is ignored.
 
-def out_degree(mat, index : Optional[int] = None):
-    if index:
-        return np.count_nonzero(mat[index, :].toarray()[0])
-    else:
-        out_degree, _ = np.histogram(mat.nonzero()[0], np.arange(mat.shape[0] + 1))
-        return out_degree
-    
+        Returns
+        ---
+        sub_matrix:
+            The sparse adjacency matrix of the largest strongly connected subgraph.
+        sub_labels:
+            The labels of each node in the subgraph.
+        """
+        _, component_labels = sparse.csgraph.connected_components(self.adjacency, directed=directed, connection=connection)
+        unique_component_labels, count = np.unique(component_labels, return_counts=True)
+        largest = unique_component_labels[np.argmax(count)]
+        indices = np.where(component_labels == largest)[0]
+        unconnected_indices = list(set(range(self.adjacency.shape[0])) - set(indices))
+        self.labels.remove_indices(unconnected_indices)
+        sub_matrix = _delete_from_csr(self.adjacency, unconnected_indices)
+        sub_graph = (sub_matrix, self.labels)
+        return sub_graph
 
-def in_degree(mat, index : Optional[int] = None):
-    if index:
-        return np.count_nonzero(mat[:, index].toarray()[0])
-    else:
-        out_degree, _ = np.histogram(mat.nonzero()[1], np.arange(mat.shape[0] + 1))
-        return out_degree
+    def compute_degree(self) -> None:
+        """
+        Fills in the degrees for self.in_degree and self.out_degree
+        """
+        self.in_degree = np.sum(self.adjacency, axis = 0)
+        self.out_degree = np.sum(self.adjacency, axis = 1)
+        
+    def in_degree(self, index : int) -> int:
+        return self.in_degree
+
+    def out_degree(self, index : int) -> int:
+        return self.out_degree
+
+    def outgoing(self, index : int) -> list[int]:
+        nodes = self.adjacency[index,:].toarray()[0]
+        return nodes[nodes != 0]
+
+    def incoming(self, index : int) -> list[int]:
+        nodes = self.adjacency[index,:].toarray()[0]
+        return nodes[nodes != 0]
+
 
 if __name__ == "__main__":
+    # US_ARTICLE_ID = 12
     mat = load_npz(SPARSE_MATRIX_PATH)
     node_list = load_node_list()
 
