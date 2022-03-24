46c702ca37c949e99956697b2444e95e9b827de2
Gus Beringer <gusberinger@gmail.com>
2022-03-24 03:06:02 +0100

    moved node list to sparse labels out of helpers

--- sparse_graph/helpers.py
+++ sparse_graph/helpers.py
@@ -1,12 +1,7 @@
 """
 Helper classes and methods.
 """
-import pickle
 from pathlib import Path
-import sqlite3
-from typing import List
-import pandas as pd
-import numpy as np
 import logging
 
 logging.basicConfig(
@@ -20,75 +15,7 @@ ROOT_PATH = Path(__file__).parent.parent
 DUMP_PATH = ROOT_PATH.joinpath("dump")
 DATABASE_PATH = DUMP_PATH.joinpath("sdow.sqlite")
 SPARSE_MATRIX_PATH = DUMP_PATH.joinpath("sparse_mat.npz")
-NODES_LIST_PATH = DUMP_PATH.joinpath("nodes_list.pickle")
+SPARSE_LABEL_PATH = DUMP_PATH.joinpath("labels.pickle")
 TESTING_MATRIX_PATH = DUMP_PATH.joinpath("testing_matrix.pickle")
 US_ARTICLE_ID = 3434750
 
-class Database(object):
-	def __init__(self):
-		if not Path(DATABASE_PATH).is_file():
-			raise IOError('Specified SQLite file "{0}" does not exist.'.format(DATABASE_PATH))
-		self.sdow_conn = sqlite3.connect(DATABASE_PATH, check_same_thread=False)
-		self.sdow_cursor = self.sdow_conn.cursor()
-
-class NodeList(object):
-	def __init__(self, custom : List = []) -> None:
-		if custom:
-			node_list = custom
-		else:
-			db = Database()
-			pages_df = pd.read_sql("SELECT id FROM pages where is_redirect=0", db.sdow_conn)
-			node_list = sorted(pages_df["id"])
-		node_dict = {article_id:index for index, article_id in enumerate(node_list)}
-		self.node_dict = node_dict
-		self.node_list = node_list
-
-	def find_index(self, article_id : int, safe = True):
-		if safe:
-			try:
-				return self.node_dict[article_id]
-			except KeyError:
-				return np.nan
-		else:
-			return self.node_dict[article_id]
-
-
-	def find_article_id(self, index : int):
-		return self.node_list[index]
-
-
-	def remove_indices(self, indices : List[int]) -> None:
-		"""Remove values from NodeList by index in place. The indexes are shifted down so the structure is maintained.
-		That is, if index 2 is removed from [(0, A), (1, B), (2, C), (3, D)), (4, E), (5, F)] the new list becomes:
-			[(0, A), (1, B), (2, D), (3, E), (4, F)]
-		"""
-		# ensure order of dataframe
-		df = pd.DataFrame(self.node_dict.items(), columns = ["label", "index"])
-		df = df.sort_values(by = ["index"])
-		df = df.drop(indices, axis=0)
-		node_list = list(df["label"])
-		self.node_dict = {article_id:index for index, article_id in enumerate(node_list)}
-		self.node_list = node_list
-
-	def __len__(self):
-		return len(self.node_dict.items())
-
-	def __str__(self):
-		return str(self.node_list)
-
-def load_node_list():
-	if Path(NODES_LIST_PATH).is_file():
-		with open(NODES_LIST_PATH, "rb") as f:
-			return pickle.load(f)
-	else:
-		logging.warning("Node list file not found. Generating...")
-		node_list = NodeList()
-		with open(NODES_LIST_PATH, "wb") as f:
-			pickle.dump(node_list, f)
-		return node_list
-
-
-if __name__ == "__main__":
-	node_list = NodeList(testing = True)
-	node_list.remove_indices([2,3])
-	print(node_list.node_dict)
--- sparse_graph/sparse_graph.py
+++ sparse_graph/sparse_graph.py
@@ -8,99 +8,142 @@ from helpers import *
 
 log = logging.getLogger(__name__)
 
+
+
+
+class SparseLabels:
+	
+	def __init__(self, new_df) -> None:
+
+		self._internal_df = new_df
+		article_id_from_index_dict = dict(zip(new_df.index, new_df["article_id"]))
+		title_from_index_dict = dict(zip(new_df.index, new_df["title"]))
+		index_from_article_id_dict = {v:k for k, v in article_id_from_index_dict.items()}
+
+		self.find_index_from_article_id = index_from_article_id_dict.get
+		self.find_article_id_from_index = article_id_from_index_dict.get
+		self.find_title_from_index = title_from_index_dict.get
+		self.find_title_from_article_id = dict(zip(new_df["article_id"], new_df["title"])).get
+		
+	def remove_indices(self, indices) -> 'SparseLabels':
+		new_df = self._internal_df.drop(indices, axis = 0)
+		return SparseLabels(new_df)
+
+
+def load_labels(from_scratch = False) -> 'SparseLabels':
+
+	# load pickled to avoid generating every time. 
+	if not from_scratch and Path(SPARSE_LABEL_PATH).is_file():
+		with open(SPARSE_LABEL_PATH, "rb") as f:
+			return pickle.load(f)
+	else:
+		log.warning("Node list file not found. Generating...")
+		
+		if not Path(DATABASE_PATH).is_file():
+			raise IOError('Specified SQLite file "{0}" does not exist.'.format(DATABASE_PATH))
+			
+		sdow_conn = sqlite3.connect(DATABASE_PATH, check_same_thread=False)
+		labels_df = pd.read_sql("SELECT id, title FROM pages where is_redirect=0", sdow_conn, columns=["article_id", "title"])
+		labels_df = labels_df.sort_values(by = ["article_id"])
+
+		labels = SparseLabels(labels_df)
+
+		with open(SPARSE_LABEL_PATH, "wb") as f:
+			pickle.dump(labels, f)
+		
+		return labels
+
+
 def _delete_from_csr(mat, indices : List[int]):
-    """
-    Parameters
-    ---
-    mat:
-        The csr sparse adjacency matrix of the graph.
-    indices:
-        The indices of the nodes in the graph that will be removed.
+	"""
+	Parameters
+	---
+	mat:
+		The csr sparse adjacency matrix of the graph.
+	indices:
+		The indices of the nodes in the graph that will be removed.
 
-    Returns
-    ---
-    mat:
-        The sparse adjacency matrix of the graph with the nodes removed.
-    """
-    if mat.shape[0] != mat.shape[1]:
-        raise ValueError("Matrix must be square.")
+	Returns
+	---
+	mat:
+		The sparse adjacency matrix of the graph with the nodes removed.
+	"""
+	if mat.shape[0] != mat.shape[1]:
+		raise ValueError("Matrix must be square.")
 
-    mask = np.ones(mat.shape[0], dtype=bool)
-    mask[indices] = False
-    return mat[mask][:,mask]
+	mask = np.ones(mat.shape[0], dtype=bool)
+	mask[indices] = False
+	return mat[mask][:,mask]
 
 class SparseGraph:
 
-    def __init__(self, adjacency, labels : Union[List[Any], NodeList]) -> None:
-        self.adjacency = adjacency
-        # self.adjacency_csc = adjacency.tocsc()
-        self.labels = NodeList(labels) if not isinstance(labels, NodeList) else labels
-        self.in_degrees = None
-        self.out_degrees = None
+	def __init__(self, adjacency, labels = None) -> None:
+		self.adjacency = adjacency
+		# self.adjacency_csc = adjacency.tocsc()
+		self.labels = SparseLabels
+		self.in_degrees = None
+		self.out_degrees = None
 
-    def get_largest_component(self, directed : bool = True, connection : str = "strong") -> 'SparseGraph':
-        """
-        Parameters
-        ---
-        matrix:
-            The sparse adjacency matrix of the graph.
-        labels:
-            The labels of each node in the graph. The index of the list corresponds to the index in the adjacency matrix.
-        direct:
-            If ``True`` the graph is treated as directed. 
-        connection:
-            If ``"strong"``, the connected components will all be strongly connected together, if ``"weak"`` the connected components will be weakly connected. If ``directed == False`` the parameter is ignored.
+	def get_largest_component(self, directed : bool = True, connection : str = "strong") -> 'SparseGraph':
+		"""
+		Parameters
+		---
+		matrix:
+			The sparse adjacency matrix of the graph.
+		labels:
+			The labels of each node in the graph. The index of the list corresponds to the index in the adjacency matrix.
+		direct:
+			If ``True`` the graph is treated as directed. 
+		connection:
+			If ``"strong"``, the connected components will all be strongly connected together, if ``"weak"`` the connected components will be weakly connected. If ``directed == False`` the parameter is ignored.
 
-        Returns
-        ---
-        sub_matrix:
-            The sparse adjacency matrix of the largest strongly connected subgraph.
-        sub_labels:
-            The labels of each node in the subgraph.
-        """
-        _, component_labels = sparse.csgraph.connected_components(self.adjacency, directed=directed, connection=connection)
-        unique_component_labels, count = np.unique(component_labels, return_counts=True)
-        largest = unique_component_labels[np.argmax(count)]
-        indices = np.where(component_labels == largest)[0]
-        unconnected_indices = list(set(range(self.adjacency.shape[0])) - set(indices))
-        self.labels.remove_indices(unconnected_indices)
-        sub_matrix = _delete_from_csr(self.adjacency, unconnected_indices)
-        sub_graph = SparseGraph(sub_matrix, self.labels)
-        return sub_graph
+		Returns
+		---
+		sub_matrix:
+			The sparse adjacency matrix of the largest strongly connected subgraph.
+		sub_labels:
+			The labels of each node in the subgraph.
+		"""
+		_, component_labels = sparse.csgraph.connected_components(self.adjacency, directed=directed, connection=connection)
+		unique_component_labels, count = np.unique(component_labels, return_counts=True)
+		largest = unique_component_labels[np.argmax(count)]
+		indices = np.where(component_labels == largest)[0]
+		unconnected_indices = list(set(range(self.adjacency.shape[0])) - set(indices))
+		self.labels.remove_indices(unconnected_indices)
+		sub_matrix = _delete_from_csr(self.adjacency, unconnected_indices)
+		sub_graph = SparseGraph(sub_matrix, self.labels)
+		return sub_graph
 
-    def compute_degree(self) -> None:
-        """
-        Fills in the degrees for self.in_degree and self.out_degree
-        """
-        self.in_degree = np.array(np.sum(self.adjacency, axis = 0))[0]
-        self.out_degree = np.array(np.sum(self.adjacency, axis = 1).transpose())[0]
-        
+	def compute_degree(self) -> None:
+		"""
+		Fills in the degrees for self.in_degree and self.out_degree
+		"""
+		self.in_degree = np.array(np.sum(self.adjacency, axis = 0))[0]
+		self.out_degree = np.array(np.sum(self.adjacency, axis = 1).transpose())[0]
+		
 
-    def outgoing_neighbors(self, index : int) -> list[int]:
-        nodes = self.adjacency[index,:].toarray()[0]
-        return np.nonzero(nodes)[0]
+	def outgoing_neighbors(self, index : int) -> list[int]:
+		nodes = self.adjacency[index,:].toarray()[0]
+		return np.nonzero(nodes)[0]
 
-    def incoming_neighbors(self, index : int) -> list[int]:
-        nodes = self.adjacency[:, index].toarray()[0]
-        return np.nonzero(nodes)[0]
+	def incoming_neighbors(self, index : int) -> list[int]:
+		nodes = self.adjacency[:, index].toarray()[0]
+		return np.nonzero(nodes)[0]
 
-    def remove_indices(self, indices : List[int]):
-        new_adjacency = _delete_from_csr(self.adjacency, indices)
-        new_labels = self.labels.remove_indices(indices)
+	def remove_indices(self, indices : List[int]):
+		new_adjacency = _delete_from_csr(self.adjacency, indices)
+		new_labels = self.labels.remove_indices(indices)
 
 
 
 if __name__ == "__main__":
-    # US_ARTICLE_ID = 12
-    adj = load_npz(SPARSE_MATRIX_PATH)
-    node_list = load_node_list()
-    graph = SparseGraph(adj, node_list)
-    log.info("Imported graph")
-    graph.compute_degree()
-    log.info("Computed degrees.")
-    print(graph.out_degree.shape)
-    print(type(graph.out_degree))
-    print(graph.out_degree)
-    print(int(graph.out_degree[0]))
-    us_index = node_list.find_index(US_ARTICLE_ID)
-    
+	# US_ARTICLE_ID = 12
+	adj = load_npz(SPARSE_MATRIX_PATH)
+	node_list = load_node_list()
+	graph = SparseGraph(adj, node_list)
+	log.info("Imported graph")
+	graph.compute_degree()
+	log.info("Computed degrees.")
+	
+
+	print()
