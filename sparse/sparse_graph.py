7f92be048ae069387f3ccc78284593af7a17a99e
Gus Beringer <berin013@umn.edu>
2022-04-05 11:58:30 -0500

    enforced PEP8 rules

--- sparse/sparse_graph.py
+++ sparse/sparse_graph.py
@@ -6,8 +6,7 @@ import pandas as pd
 import numpy as np
 from typing import Dict, List
 from scipy import sparse
-from helpers import *
-
+from helpers import SPARSE_LABEL_PATH, DATABASE_PATH, US_ARTICLE_ID, SPARSE_MATRIX_PATH
 log = logging.getLogger(__name__)
 
 
@@ -27,7 +26,7 @@ class SparseLabels:
         self.find_title_from_article_id = title_from_article_id.get
 
     def remove_indices(self, indices) -> SparseLabels:
-        new_df = self._internal_df.drop(indices, axis = 0)
+        new_df = self._internal_df.drop(indices, axis=0)
         new_df = new_df.reset_index(drop=True)
         return SparseLabels(new_df)
 
@@ -51,12 +50,13 @@ def _delete_from_csr(mat: sparse.csr_matrix, indices: List[int]) -> sparse.csr_m
 
     mask = np.ones(mat.shape[0], dtype=bool)
     mask[indices] = False
-    return mat[mask][:,mask]
+    return mat[mask][:, mask]
+
 
 class SparseGraph:
 
     def __init__(self, adjacency: sparse.csr_matrix,
-                       labels: SparseLabels) -> None:
+                 labels: SparseLabels) -> None:
         self.adjacency = adjacency
         self.labels = labels
         self._in_degree = None
@@ -69,11 +69,14 @@ class SparseGraph:
         matrix:
             The sparse adjacency matrix of the graph.
         labels:
-            The labels of each node in the graph. The index of the list corresponds to the index in the adjacency matrix.
+            The labels of each node in the graph.
+            The index of the list corresponds to the index in the adjacency matrix.
         direct:
             If ``True`` the graph is treated as directed.
         connection:
-            If ``"strong"``, the connected components will all be strongly connected together, if ``"weak"`` the connected components will be weakly connected. If ``directed == False`` the parameter is ignored.
+            If ``"strong"``, the connected components will all be strongly connected together.
+            If ``"weak"`` the connected components will be weakly connected.
+            If ``directed == False`` the parameter is ignored.
 
         Returns
         ---
@@ -82,7 +85,11 @@ class SparseGraph:
         sub_labels:
             The labels of each node in the subgraph.
         """
-        _, component_labels = sparse.csgraph.connected_components(self.adjacency, directed=directed, connection=connection)
+        _, component_labels = sparse.csgraph.connected_components(
+            self.adjacency,
+            directed=directed,
+            connection=connection
+        )
         unique_component_labels, count = np.unique(component_labels, return_counts=True)
         largest = unique_component_labels[np.argmax(count)]
         indices = np.where(component_labels == largest)[0]
@@ -103,7 +110,6 @@ class SparseGraph:
         else:
             return self._in_degree[index]
 
-
     def out_degree(self, index: int) -> int:
         if self._out_degree is None:
             raise ValueError("Degrees not computed. Must call self.compute_degree on instance first.")
@@ -111,7 +117,7 @@ class SparseGraph:
             return self._out_degree[index]
 
     def outgoing_neighbors(self, index: int) -> np.ndarray:
-        nodes = self.adjacency[index,:].toarray()[0]
+        nodes = self.adjacency[index, :].toarray()[0]
         return np.nonzero(nodes)[0]
 
     def incoming_neighbors(self, index: int) -> np.ndarray:
@@ -124,28 +130,27 @@ class SparseGraph:
         return SparseGraph(new_adjacency, new_labels)
 
 
-
-def load_wiki_labels(from_scratch = False) -> SparseLabels:
+def load_wiki_labels(from_scratch=False) -> SparseLabels:
     # load pickled to avoid generating every time.
-    if not from_scratch and Path(SPARSE_LABEL_PATH).is_file():
+    if not from_scratch and SPARSE_LABEL_PATH.is_file():
         with open(SPARSE_LABEL_PATH, "rb") as f:
             return pickle.load(f)
     else:
         log.warning("Node list file not found. Generating...")
 
-        if not Path(DATABASE_PATH).is_file():
+        if not DATABASE_PATH.is_file():
             raise IOError('Specified SQLite file "{0}" does not exist.'.format(DATABASE_PATH))
 
         sdow_conn = sqlite3.connect(DATABASE_PATH, check_same_thread=False)
         labels_df = pd.read_sql("SELECT id, title FROM pages where is_redirect=0", sdow_conn)
         labels_df = labels_df.rename(columns={"id": "article_id"})
-        labels_df = labels_df.sort_values(by = ["article_id"])
+        labels_df = labels_df.sort_values(by=["article_id"])
         labels = SparseLabels(labels_df)
 
         with open(SPARSE_LABEL_PATH, "wb") as f:
             pickle.dump(labels, f)
-
         return labels
+
 
 def load_wiki_graph() -> SparseGraph:
     labels: SparseLabels = load_wiki_labels()
@@ -153,8 +158,6 @@ def load_wiki_graph() -> SparseGraph:
         matrix: sparse.csr_matrix = sparse.load_npz(f)
     graph = SparseGraph(matrix, labels)
     return graph
-
-
 
 
 if __name__ == "__main__":
