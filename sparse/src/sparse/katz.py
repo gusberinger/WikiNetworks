ab0d22af6346ca1ba287d55621740bbf286c9275
Gus Beringer <berin013@umn.edu>
2022-04-05 14:43:55 -0500

    added damping_factor options

--- sparse/src/sparse/katz.py
+++ sparse/src/sparse/katz.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from .random_graph import random_sparse_graph
 from sknetwork.ranking import Katz
+import random
 
-
-def katz_centrality(graph):
-    close = Katz()
+def katz_centrality(graph, damping_factor: float = 0.5):
+    close = Katz(damping_factor=damping_factor, path_length=40)
     scores = close.fit_transform(graph.adjacency)
     rows = [(graph.labels.find_title_from_index(i),
              graph.labels.find_article_id_from_index(i),
