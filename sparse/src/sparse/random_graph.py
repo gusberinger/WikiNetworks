3fed252ee73f380e53f094ad9c0740d9926aa9fe
Gus Beringer <berin013@umn.edu>
2022-04-05 16:02:33 -0500

    implemented function to convert networkx graphs

--- sparse/src/sparse/random_graph.py
+++ sparse/src/sparse/random_graph.py
@@ -18,16 +18,21 @@ def random_integers():
         yield random.randint(0, 9000000)
 
 
-def random_sparse_graph(nodes, p, seed):
-    random.seed(seed)
-    G = nx.binomial_graph(nodes, p, directed=True)
-    adj = nx.adjacency_matrix(G).astype(bool)
+def from_networkx(G):
+    n = len(G)
+    adj = nx.to_scipy_sparse_array(G).astype(bool)
     article_titles = titles()
     article_ids = random_integers()
-    rows = itertools.islice(zip(article_titles, article_ids), nodes)
+    rows = itertools.islice(zip(article_titles, article_ids), n)
     label_df = pd.DataFrame(rows, columns=["title", "article_id"])
     graph = SparseGraph(adj, label_df)
     return graph
+
+
+def random_sparse_graph(nodes, p, seed):
+    random.seed(seed)
+    G = nx.binomial_graph(nodes, p, directed=True)
+    return from_networkx(G)
 
 
 if __name__ == "__main__":
