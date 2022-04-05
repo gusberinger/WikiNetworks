73ac27d9da3fa05101b543407d3b81005469fb31
Gus Beringer <berin013@umn.edu>
2022-04-05 16:01:54 -0500

    working test case for katz centrality

--- sparse/tests/test_katz_centrality.py
+++ sparse/tests/test_katz_centrality.py
@@ -1,17 +1,21 @@
 import numpy as np
 import scipy as sp
-import sparse
 import unittest
 import networkx as nx
+from sparse import random_graph
 from sparse.katz import katz_centrality
+from scipy.stats import pearsonr
 
+class TestKatzCentrality(unittest.TestCase):
+
+    def test_kite(self):
+        nx_graph = nx.krackhardt_kite_graph()
+        graph = random_graph.from_networkx(nx_graph)
+
+        sparse_scores = list(katz_centrality(graph, alpha=.1, beta=1))
+        nx_scores = list(nx.katz_centrality(nx_graph).values())
+        cor = pearsonr(sparse_scores, nx_scores)[0]
+        self.assertGreater(cor, .9999)
 
 if __name__ == "__main__":
-    graph = sparse.random_sparse_graph(100, .01)
-    report = list(katz_centrality(graph, damping_factor=0.1).score)
-    nx_graph = graph.to_networkx()
-    nx_scores = nx.katz_centrality(nx_graph).values()
-    # for x, y in zip(report, nx_scores, strict=True):
-    #     print(x, y)
-    cor = np.corrcoef(report, list(nx_scores))
-    print(cor)
+    unittest.main()
