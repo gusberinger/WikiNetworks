789c74a7fe00b45728d2a6ea02dd0ccf62fc6729
Gus Beringer <berin013@umn.edu>
2022-04-05 16:02:14 -0500

    used linear solving to find katz centrality

--- sparse/src/sparse/katz.py
+++ sparse/src/sparse/katz.py
@@ -1,13 +1,18 @@
+import numpy as np
 import pandas as pd
+import scipy
 from .random_graph import random_sparse_graph
 from sknetwork.ranking import Katz
-import random
+import scipy.sparse
+import scipy.sparse.linalg
 
-def katz_centrality(graph, damping_factor: float = 0.5):
-    close = Katz(damping_factor=damping_factor, path_length=40)
-    scores = close.fit_transform(graph.adjacency)
-    rows = [(graph.labels.find_title_from_index(i),
-             graph.labels.find_article_id_from_index(i),
-             score) for i, score in enumerate(scores)]
-    df = pd.DataFrame(rows, columns=["title", "article_id", "score"])
-    return df
+
+def katz_centrality(graph, alpha: float = 0.1, beta: float = 1):
+    """Adapted from networkx for sparse matrices"""
+    n = graph.size
+    A = graph.adjacency
+    b = np.ones((n, 1)) * float(beta)
+    term = scipy.sparse.eye(n, n) - (alpha * A)
+    centrality = scipy.sparse.linalg.spsolve(scipy.sparse.eye(n, n) - (alpha * A), b)
+    norm = np.sign(sum(centrality)) * np.linalg.norm(centrality)
+    return map(float, centrality / norm)
