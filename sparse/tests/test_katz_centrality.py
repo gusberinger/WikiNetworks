595cdfb5e155011645f6d71666549b44d9f20ff8
Gus Beringer <berin013@umn.edu>
2022-04-06 16:17:38 -0500

    implemented example test from youtube

--- sparse/tests/test_katz_centrality.py
+++ sparse/tests/test_katz_centrality.py
@@ -8,14 +8,14 @@ from scipy.stats import pearsonr
 
 class TestKatzCentrality(unittest.TestCase):
 
-    def test_kite(self):
-        nx_graph = nx.krackhardt_kite_graph()
-        graph = random_graph.from_networkx(nx_graph)
-
-        sparse_scores = list(katz_centrality(graph, alpha=.1, beta=1))
-        nx_scores = list(nx.katz_centrality(nx_graph).values())
-        cor = pearsonr(sparse_scores, nx_scores)[0]
-        self.assertGreater(cor, .9999)
+    def test_example(self):
+        adj = np.matrix([[0, 1, 1, 1, 0], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 0], [0, 1, 1, 0, 0]])
+        nxG = nx.from_numpy_matrix(adj)
+        G = random_graph.from_networkx(nxG)
+        sparse_scores = katz_centrality(G, alpha=0.25, beta=0.2, normalized=False)
+        real_scores = [1.1428571428571426, 1.3142857142857138, 1.3142857142857138, 1.1428571428571426, 0.857142857142857]
+        cor = pearsonr(sparse_scores, real_scores)[0]
+        self.assertGreater(cor, .99)
 
 if __name__ == "__main__":
     unittest.main()
