2601fdd98ed688593aff47cec135a5b38f072670
Gus Beringer <berin013@umn.edu>
2022-04-06 16:13:33 -0500

    fixed convergence errors

--- sparse/src/sparse/katz.py
+++ sparse/src/sparse/katz.py
@@ -9,21 +9,22 @@ import logging
 
 
 def katz_centrality(graph, alpha: float = 0.1, beta: float = 1,
-                    max_iter = 1000, tol=1.0e-6, normalized = True):
+                    max_iter = 10000, tol=1.0e-6, normalized = True):
     A = graph.adjacency.transpose()
     n = graph.size
     e = np.ones((n, 1))
     last = e.copy()
     for k in range(max_iter):
-        current = alpha * A * last + beta * e
-        error = sum((current[i] - last[i] for i in range(n)))
+        current = alpha * A.dot(last) + beta * e
+        error = sum((abs(current[i] - last[i]) for i in range(n)))
         if error < n * tol:
-            logging.info(f"Converged in {k} iterations")
+            logging.info(f"Converged in {k:,} iterations")
+            centrality = current.flatten().tolist()
             if normalized:
-                norm = np.sign(sum(current)) * np.linalg.norm(current)
-                return map(float, current / norm)
-
+                norm = np.sign(sum(centrality)) * np.linalg.norm(centrality)
+                return map(float, centrality / norm)
             else:
-                return current
+                return centrality
+        last = current.copy()
 
     raise ValueError(f"Failed to converge in {max_iter} iterations.")
