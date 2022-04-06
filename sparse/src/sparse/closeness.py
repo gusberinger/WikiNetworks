61c782cacee3d72351e8eec19570180d514014d8
Gus Beringer <berin013@umn.edu>
2022-04-05 22:33:54 -0500

    implemented estimated closeness centrality

---
+++ sparse/src/sparse/closeness.py
@@ -0,0 +1,12 @@
+from .breadth_first_search import distance_from
+import random
+import numpy as np
+
+def closeness_centrality(k, graph):
+    n = graph.size
+    total = np.zeros(n)
+    for _ in range(k):
+        i = random.randint(0, n - 1)
+        sssp = distance_from(graph, i)
+        total += (n / (k * (n-1))) * sssp
+    return 1 / total
