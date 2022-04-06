6fa26284e465e40fe0b96079a2bfff5c33cd3142
Gus Beringer <berin013@umn.edu>
2022-04-06 15:25:29 -0500

    implemented katz centrality using power iteration

--- sparse/src/sparse/katz.py
+++ sparse/src/sparse/katz.py
@@ -5,14 +5,25 @@ from .random_graph import random_sparse_graph
 from sknetwork.ranking import Katz
 import scipy.sparse
 import scipy.sparse.linalg
+import logging
 
 
-def katz_centrality(graph, alpha: float = 0.1, beta: float = 1):
-    """Adapted from networkx for sparse matrices"""
+def katz_centrality(graph, alpha: float = 0.1, beta: float = 1,
+                    max_iter = 1000, tol=1.0e-6, normalized = True):
+    A = graph.adjacency.transpose()
     n = graph.size
-    A = graph.adjacency
-    b = np.ones((n, 1)) * float(beta)
-    term = scipy.sparse.eye(n, n) - (alpha * A)
-    centrality = scipy.sparse.linalg.spsolve(scipy.sparse.eye(n, n) - (alpha * A), b)
-    norm = np.sign(sum(centrality)) * np.linalg.norm(centrality)
-    return map(float, centrality / norm)
+    e = np.ones((n, 1))
+    last = e.copy()
+    for k in range(max_iter):
+        current = alpha * A * last + beta * e
+        error = sum((current[i] - last(i) for i in range(n)))
+        if error < n * tol:
+            logging.info(f"Converged in {k} iterations")
+            if normalized:
+                norm = np.sign(sum(current)) * np.linalg.norm(current)
+                return map(float, current / norm)
+
+            else:
+                return current
+
+    raise ValueError(f"Failed to converge in {max_iter} iterations.")
