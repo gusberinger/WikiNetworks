4d40cb4a66626401437d69ffb1ac71814a139ef0
Gus Beringer <gusberinger@gmail.com>
2022-04-06 01:39:30 +0100

    changed function name

--- sparse/src/sparse/__init__.py
+++ sparse/src/sparse/__init__.py
@@ -1,4 +1,4 @@
-from .random_graph import random_sparse_graph
+from .random_graph import random_sparse_graph, from_networkx
 from .sparse_graph import SparseGraph
 from .katz import katz_centrality
-from .closeness import breadth_first_search_level
+from .closeness import distance_from
