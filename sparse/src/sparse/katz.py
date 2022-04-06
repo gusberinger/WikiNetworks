cc4f33489f215034485fe9c835c13ca2afa187f9
Gus Beringer <berin013@umn.edu>
2022-04-06 16:37:06 -0500

    removed extra imports

--- sparse/src/sparse/katz.py
+++ sparse/src/sparse/katz.py
@@ -1,11 +1,4 @@
 import numpy as np
-import pandas as pd
-import scipy
-from .random_graph import random_sparse_graph
-from sknetwork.ranking import Katz
-import scipy.sparse
-import scipy.sparse.linalg
-import logging
 
 
 def katz_centrality(graph, alpha: float = 0.1, beta: float = 1,
@@ -18,7 +11,6 @@ def katz_centrality(graph, alpha: float = 0.1, beta: float = 1,
         current = alpha * A.dot(last) + beta * e
         error = sum((abs(current[i] - last[i]) for i in range(n)))
         if error < n * tol:
-            logging.info(f"Converged in {k:,} iterations")
             centrality = current.flatten().tolist()
             if normalized:
                 norm = np.sign(sum(centrality)) * np.linalg.norm(centrality)
