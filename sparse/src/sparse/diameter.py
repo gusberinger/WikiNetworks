64e2bc254d841d908b3570c7cf9f574d928c9c5f
Gus Beringer <berin013@umn.edu>
2022-04-07 11:23:08 -0500

    added comments explaining algorithm

--- sparse/src/sparse/diameter.py
+++ sparse/src/sparse/diameter.py
@@ -1,14 +1,29 @@
 import random
-from scipy import rand
+
+import numpy as np
 from .breadth_first_search import distance_from
 
 
 def estimate_diameter(graph, k):
-    U = {}
+    eccentricity = {}
+    end = {}
     n = graph.size
+    d_est = float("inf")
     for i in range(1, k+1):
         if i == 1:
             v = random.randint(0, n-1)
         else:
-            pass
-        U[v] = distance_from(graph, v)
+            # choose furthest from set of proccessed nodes
+            # set of proccesesd nodes are the keys in eccentricity
+            # hence, the furthest from set of processed nodes is the leaf with largest eccentricity
+            # the location of the leaf is held in the end dictionary
+            v = end[max(eccentricity.items(), key=lambda x: x[1])[0]]
+
+        distances = distance_from(graph, v) # bfs
+        furthest = np.argmax(distances)
+        end[v] = furthest
+        eccentricity[v] = max(distance_from(graph, v))
+        d_est = min(d_est, eccentricity[v])
+        pass
+
+    print(d_est)
