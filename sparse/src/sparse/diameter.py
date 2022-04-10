d31077e22133c7ae898e74d94351ed7afb61c84c
Gus Beringer <berin013@umn.edu>
2022-04-10 16:57:51 -0500

    changed initial estimate to -inf

--- sparse/src/sparse/diameter.py
+++ sparse/src/sparse/diameter.py
@@ -1,17 +1,17 @@
 import random
-
 import numpy as np
 from .breadth_first_search import distance_from
 
 
 def estimate_diameter(graph, k):
+    random.seed(10)
     eccentricity = {}
     end = {}
     n = graph.size
-    diameter_estimate = float("inf")
-    for i in range(1, k+1):
+    diameter_estimate = -float("inf")
+    for i in range(1, k + 1):
         if i == 1:
-            v = random.randint(0, n-1)
+            v = random.randint(0, n - 1)
         else:
             # choose furthest from set of proccessed nodes
             # set of proccesesd nodes are the keys in eccentricity
@@ -19,10 +19,9 @@ def estimate_diameter(graph, k):
             # the location of the leaf is held in the end dictionary
             v = end[max(eccentricity.items(), key=lambda x: x[1])[0]]
 
-        distances = distance_from(graph, v) # bfs
+        distances = distance_from(graph, v)  # bfs
         furthest = np.argmax(distances)
         end[v] = furthest
         eccentricity[v] = max(distance_from(graph, v))
         diameter_estimate = max(diameter_estimate, eccentricity[v])
-
     return diameter_estimate
