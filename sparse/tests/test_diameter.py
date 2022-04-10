1869dd4eda3749db68b9066fc52bb7afbc2bde73
Gus Beringer <berin013@umn.edu>
2022-04-10 16:56:53 -0500

    script to test diameter

--- sparse/tests/test_diameter.py
+++ sparse/tests/test_diameter.py
@@ -1,13 +1,10 @@
 import networkx as nx
 from sparse.diameter import estimate_diameter
-
 from sparse.random_graph import from_networkx
 
 if __name__ == "__main__":
     nxG = nx.watts_strogatz_graph(1000, 10, .01, seed=10)
     G = from_networkx(nxG)
-    # nx.draw_circular(nxG)
-    # plt.show()
-    d = estimate_diameter(G, 50)
-    real_d = nx.diameter(nxG)
-    print(real_d)
+    estimate = estimate_diameter(G, 100)
+    real_diameter = nx.diameter(nxG)
+    print(estimate, real_diameter)
