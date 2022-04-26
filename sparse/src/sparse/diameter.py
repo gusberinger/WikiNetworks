3720c213ee0468a791489be4481318f1dff346d1
Gus Beringer <berin013@umn.edu>
2022-04-26 17:56:36 -0500

    added radius option

--- sparse/src/sparse/diameter.py
+++ sparse/src/sparse/diameter.py
@@ -3,7 +3,7 @@ import numpy as np
 from .breadth_first_search import distance_from
 
 
-def estimate_diameter(graph, k):
+def estimate_diameter(graph, k: int):
     random.seed(10)
     n = graph.size
     distances = [float("inf")] * n
@@ -19,3 +19,21 @@ def estimate_diameter(graph, k):
         distances = list(map(min, zip(bfs_result, distances)))
         diameter_estimate = max(diameter_estimate, max(bfs_result))
     return diameter_estimate
+
+# TODO: eliminate code repition
+def estimate_radius(graph, k: int):
+    random.seed(10)
+    n = graph.size
+    distances = [float("inf")] * n
+    radius_estimate = float("inf")
+    for i in range(1, k + 1):
+        if i == 1:
+            v = random.randint(0, n - 1)
+        else:
+            # choose furthest from set of proccessed nodes
+            v = np.argmax(distances)
+
+        bfs_result = distance_from(graph, v)  # bfs
+        distances = list(map(min, zip(bfs_result, distances)))
+        radius_estimate = min(radius_estimate, max(bfs_result))
+    return radius_estimate
