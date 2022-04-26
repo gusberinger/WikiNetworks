06a06b3a679dd4abd8ba7392baf642e0b5987045
Gus Beringer <berin013@umn.edu>
2022-04-26 17:54:16 -0500

    implemented diameter

--- sparse/src/sparse/diameter.py
+++ sparse/src/sparse/diameter.py
@@ -5,23 +5,17 @@ from .breadth_first_search import distance_from
 
 def estimate_diameter(graph, k):
     random.seed(10)
-    eccentricity = {}
-    end = {}
     n = graph.size
+    distances = [float("inf")] * n
     diameter_estimate = -float("inf")
     for i in range(1, k + 1):
         if i == 1:
             v = random.randint(0, n - 1)
         else:
             # choose furthest from set of proccessed nodes
-            # set of proccesesd nodes are the keys in eccentricity
-            # hence, the furthest from set of processed nodes is the leaf with largest eccentricity
-            # the location of the leaf is held in the end dictionary
-            v = end[max(eccentricity.items(), key=lambda x: x[1])[0]]
+            v = np.argmax(distances)
 
-        distances = distance_from(graph, v)  # bfs
-        furthest = np.argmax(distances)
-        end[v] = furthest
-        eccentricity[v] = max(distance_from(graph, v))
-        diameter_estimate = max(diameter_estimate, eccentricity[v])
+        bfs_result = distance_from(graph, v)  # bfs
+        distances = list(map(min, zip(bfs_result, distances)))
+        diameter_estimate = max(diameter_estimate, max(bfs_result))
     return diameter_estimate
--- sparse/tests/test_diameter.py
+++ sparse/tests/test_diameter.py
@@ -5,6 +5,6 @@ from sparse.random_graph import from_networkx
 if __name__ == "__main__":
     nxG = nx.watts_strogatz_graph(1000, 10, .01, seed=10)
     G = from_networkx(nxG)
-    estimate = estimate_diameter(G, 100)
+    estimate = estimate_diameter(G, 10)
     real_diameter = nx.diameter(nxG)
     print(estimate, real_diameter)
