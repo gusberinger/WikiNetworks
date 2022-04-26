73a0a428276bbcfca73e93be69e4bc099db42649
Gus Beringer <berin013@umn.edu>
2022-04-26 18:07:58 -0500

    estimate diameter and radius at same time

--- sparse/src/sparse/diameter.py
+++ sparse/src/sparse/diameter.py
@@ -8,23 +8,6 @@ def estimate_diameter(graph, k: int):
     n = graph.size
     distances = [float("inf")] * n
     diameter_estimate = -float("inf")
-    for i in range(1, k + 1):
-        if i == 1:
-            v = random.randint(0, n - 1)
-        else:
-            # choose furthest from set of proccessed nodes
-            v = np.argmax(distances)
-
-        bfs_result = distance_from(graph, v)  # bfs
-        distances = list(map(min, zip(bfs_result, distances)))
-        diameter_estimate = max(diameter_estimate, max(bfs_result))
-    return diameter_estimate
-
-# TODO: eliminate code repition
-def estimate_radius(graph, k: int):
-    random.seed(10)
-    n = graph.size
-    distances = [float("inf")] * n
     radius_estimate = float("inf")
     for i in range(1, k + 1):
         if i == 1:
@@ -35,5 +18,6 @@ def estimate_radius(graph, k: int):
 
         bfs_result = distance_from(graph, v)  # bfs
         distances = list(map(min, zip(bfs_result, distances)))
-        radius_estimate = min(radius_estimate, max(bfs_result))
-    return radius_estimate
+        diameter_estimate = max(diameter_estimate, max(bfs_result))
+        radius_estimate = min(diameter_estimate, max(bfs_result))
+    return diameter_estimate, radius_estimate
--- sparse/tests/test_diameter.py
+++ sparse/tests/test_diameter.py
@@ -1,10 +1,10 @@
 import networkx as nx
-from sparse.diameter import estimate_diameter
+from sparse.diameter import estimate_diameter, estimate_radius
 from sparse.random_graph import from_networkx
 
 if __name__ == "__main__":
-    nxG = nx.watts_strogatz_graph(1000, 10, .01, seed=10)
+    nxG = nx.watts_strogatz_graph(1000, 10, .01, seed=12)
     G = from_networkx(nxG)
-    estimate = estimate_diameter(G, 10)
-    real_diameter = nx.diameter(nxG)
+    estimate = estimate_radius(G, 10)
+    real_diameter = nx.radius(nxG)
     print(estimate, real_diameter)
