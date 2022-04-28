8135f3406d1d133777a20bfdb6f7cc8e527ab073
Gus Beringer <gusberinger@gmail.com>
2022-04-28 21:32:33 +0100

    changed attenuation factor

--- wikipedia/calculate_katz.py
+++ wikipedia/calculate_katz.py
@@ -7,7 +7,7 @@ DEGREE_REPORT = REPORT_PATH.joinpath("degree_centrality.parquet")
 if __name__ == "__main__":
     G = load_wikipedia()
     G = G.get_largest_component()
-    scores = sparse.katz_centrality(G, alpha=.1, max_iter=5)
+    scores = sparse.katz_centrality(G, alpha=0.0000007)
     df = G.labels._internal_df
     df["katz_centrality"] = scores
     df.to_parquet(DEGREE_REPORT)
